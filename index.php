616003b236b260eae20b4d5216d3c9c039905735
Hugo Hirsch <github@hugo-hirsch.de>
2026-03-08 18:03:00 +0100

    Proper HTML5

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
-<?php
-echo 'Hoch steht er am '.date('Y-m-d H:i:s').' .... aber richtig ;)';
-?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hoch Steht Er</title>
+</head>
+<body>
+    <header>
+        <h1>Welcome</h1>
+    </header>
+    <main>
+        <section>
+            <p>
+              <?php
+              echo 'Hoch steht er am '.date('Y-m-d H:i:s').' .... aber richtig ;)';
+              ?>
+            </p>
+        </section>
+    </main>
+    <footer>
+        <p>&copy; 2000-<?php echo date('Y'); ?> https://www.hochstehter.de</p>
+    </footer>
+</body>
+</html>
